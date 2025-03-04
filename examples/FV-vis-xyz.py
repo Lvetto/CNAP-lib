Fixed a bug where the incorrect axis object was saved for 3d plots

examples/FV-vis-xyz.py
@@ -5,13 +5,16 @@ from Graphs.FigureBuilder import *
 from Graphs.Plots import *
 from Graphs.DataClasses import *
 from random import random
+from old.pp_io import read_xyz
 
-# create a periodic lattice
-a = 1
-lattice = np.array(cubic_lattice_from_cell(1, 5, fcc_cell(a)))
+# read data from file
+filepath = r"data/tail.xyz"
+elements, points = read_xyz(filepath)
+
+cutoff = 2.93
 
 # make a graph to represent the system
-g = Graph(adjacency_matrix(0, lattice, 0.8*a), node_positions=lattice)
+g = Graph(adjacency_matrix(0, points, cutoff), node_positions=points)
 
 # create feature vectors, group them and format the data
 fvs = get_feature_vectors(g)
@@ -23,14 +26,14 @@ fig2 = [FigureBuilder((1,2), title=key) for key in keys]#FigureBuilder((2, len(k
 
 # some data for the plots
 colors = [(random(), random(), random()) for _ in grouped]
-pointclouds = [PointCloud(lattice[i], size=100, color=j, label=k, alpha=1) for i,j,k in zip(grouped, colors, keys)]
+pointclouds = [PointCloud(points[i], size=100, color=j, label=k, alpha=1) for i,j,k in zip(grouped, colors, keys)]
 
 # add subplots and show them
-fig.add_plot(better_particle_plot, plot_pos=0, points=pointclouds, hide_frames=False, title="", legend=True)
+fig.add_plot(better_particle_plot, ax=fig[0], points=pointclouds, hide_frames=False, title="", legend=True)
 
 # draw plots for each unique structure and its distribution in 3d space
 for n, key, inds in zip(range(len(grouped)), keys, grouped):
-    fig2[n].add_plot(graph_plot, plot_pos=0, graph=make_neighbors_subgraph(g, inds[0]), pointsize=300, linesize=1, linealpha=0.6, pointalpha=1, title=key)
-    fig2[n].add_plot(graph_plot, plot_pos=1, graph=g.make_subgraph(inds), pointsize=300, linesize=1, linealpha=0.6, pointalpha=1, title=key, pointcolor=colors[n])
+    fig2[n].add_plot(graph_plot, ax=fig2[n][0], graph=make_neighbors_subgraph(g, inds[0]), pointsize=300, linesize=1, linealpha=0.6, pointalpha=1, title=key)
+    fig2[n].add_plot(graph_plot, ax=fig2[n][1], graph=g.make_subgraph(inds), pointsize=300, linesize=1, linealpha=0.6, pointalpha=1, title=key, pointcolor=colors[n])
 
 plt.show()