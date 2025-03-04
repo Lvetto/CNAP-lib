d7e7a49631b7e48bb30b3b2661933c171fa773d7
lvettore02@gmail.com <lvettore02@gmail.com>
2025-03-04 15:04:32 +0100

    Fixed examples with new constructor for plots

--- examples/FV-vis-xyz.py
+++ examples/FV-vis-xyz.py
@@ -6,12 +6,13 @@ from Graphs.Plots import *
 from Graphs.DataClasses import *
 from random import random
 from old.pp_io import read_xyz
+import imageio.v2 as imageio
 
 # read data from file
-filepath = r"data/tail.xyz"
+filepath = r"data/fcc5.xyz"
 elements, points = read_xyz(filepath)
 
-cutoff = 2.93
+cutoff = 0.8
 
 # make a graph to represent the system
 g = Graph(adjacency_matrix(0, points, cutoff), node_positions=points)
@@ -20,20 +21,28 @@ g = Graph(adjacency_matrix(0, points, cutoff), node_positions=points)
 fvs = get_feature_vectors(g)
 keys, grouped = group_fvs(fvs)
 
-# create figures
+print(f"Unique LAEs found: {keys}")
+
+# some config for the figures
+fps=8
+rot_steps = (0, 4, 0)
+rot0 = (20, 0, 0)
+
+# draw a figure showing the spatial distribution of FVs
 fig = FigureBuilder((1, 1))
-fig2 = [FigureBuilder((1,2), title=key) for key in keys]#FigureBuilder((2, len(keys)), title="LAEs")
-
-# some data for the plots
 colors = [(random(), random(), random()) for _ in grouped]
-pointclouds = [PointCloud(points[i], size=100, color=j, label=k, alpha=1) for i,j,k in zip(grouped, colors, keys)]
+pointclouds = [PointCloud(points[i], size=200, color=j, label=k, alpha=1) for i,j,k in zip(grouped, colors, keys)]
+fig.add_plot(rotating_particle_plot, plot_pos=0, points=pointclouds, hide_frames=False, title="", legend=True, rot_steps=rot_steps, rot0=rot0)
+fig.show(frames=90, save=True, path=rf"data/out/all.gif", fps=fps)
+del fig
+print(f"saved figure: all")
 
-# add subplots and show them
-fig.add_plot(better_particle_plot, ax=fig[0], points=pointclouds, hide_frames=False, title="", legend=True)
-
-# draw plots for each unique structure and its distribution in 3d space
-for n, key, inds in zip(range(len(grouped)), keys, grouped):
-    fig2[n].add_plot(graph_plot, ax=fig2[n][0], graph=make_neighbors_subgraph(g, inds[0]), pointsize=300, linesize=1, linealpha=0.6, pointalpha=1, title=key)
-    fig2[n].add_plot(graph_plot, ax=fig2[n][1], graph=g.make_subgraph(inds), pointsize=300, linesize=1, linealpha=0.6, pointalpha=1, title=key, pointcolor=colors[n])
-
-plt.show()
+# save figures for different FVs
+for key, inds, color in zip(keys, grouped, colors):
+    fig = FigureBuilder((1, 2), title=key)
+    fig.add_plot(rotating_graph_plot, plot_pos=0, graph=make_neighbors_subgraph(g, inds[0]), rot_steps=rot_steps, rot0=rot0 ,hide_frames=True, pointsize=100, linesize=1, linealpha=0.6, pointalpha=0.8)
+    fig.add_plot(rotating_graph_plot, plot_pos=1, graph=g.make_subgraph(inds), rot_steps=rot_steps, rot0=rot0 ,hide_frames=True, pointsize=50, linesize=1, linealpha=0.6, pointalpha=0.8, pointcolor=color)
+    
+    fig.show(frames=90, save=True, path=rf"data/out/{key}.gif", fps=fps)
+    del fig
+    print(f"saved figure: {key}")
--- examples/FV-vis.py
+++ examples/FV-vis.py
@@ -8,10 +8,10 @@ from random import random
 
 # create a periodic lattice
 a = 1
-lattice = np.array(cubic_lattice_from_cell(1, 5, fcc_cell(a)))
+lattice = np.array(cubic_lattice_from_cell(1, 5, bcc_cell(a)))
 
 # make a graph to represent the system
-g = Graph(adjacency_matrix(0, lattice, 0.8*a), node_positions=lattice)
+g = Graph(adjacency_matrix(0, lattice, 1.1*a), node_positions=lattice)
 
 # create feature vectors, group them and format the data
 fvs = get_feature_vectors(g)
@@ -23,7 +23,7 @@ fig2 = [FigureBuilder((1,2), title=key) for key in keys]#FigureBuilder((2, len(k
 
 # some data for the plots
 colors = [(random(), random(), random()) for _ in grouped]
-pointclouds = [PointCloud(lattice[i], size=100, color=j, label=k, alpha=1) for i,j,k in zip(grouped, colors, keys)]
+pointclouds = [PointCloud(lattice[i], size=300, color=j, label=k, alpha=1) for i,j,k in zip(grouped, colors, keys)]
 
 # add subplots and show them
 fig.add_plot(better_particle_plot, plot_pos=0, points=pointclouds, hide_frames=False, title="", legend=True)
